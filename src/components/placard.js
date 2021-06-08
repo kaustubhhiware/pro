f30664b2b035850618af84381fe12fa5689a564a
Kaustubh Hiware <hiwarekaustubh@gmail.com>
2021-06-09 05:58:32 +0900

    Complete placard

--- src/components/placard.js
+++ src/components/placard.js
@@ -2,7 +2,11 @@ import React from "react";
 import './base.css';
 import { Col } from 'react-bootstrap';
 
+import Bio from '../data/bio';
 import profilePic from './../images/kaustubh.png';
+import { FaGithub, FaInstagram, FaLinkedinIn, FaTwitter } from 'react-icons/fa';
+import { SiGooglescholar } from "react-icons/si";
+import { IoMail } from "react-icons/io5";
 
 
 export const Placard = () => {
@@ -13,10 +17,39 @@ export const Placard = () => {
     <img src={profilePic} className="placard-image" alt="Profile"/>
     
     <div className="placard-title">
-    Kaustubh Hiware
+      {Bio.Name}
+    </div>
+    <div className="placard-role">
+      {Bio.Role}
+    </div>
+
+    <div className="placard-location">
+      {Bio.Location}
     </div>
     
     <hr className="liner" />
+
+    <div className="social">
+    <a href={Bio.URLs.Twitter} rel="noopener noreferrer" target="_blank">
+      <FaTwitter />
+    </a>
+    <a href={Bio.URLs.LinkedIn} rel="noopener noreferrer" target="_blank">
+      <FaLinkedinIn />
+    </a>
+    <a href={Bio.URLs.Instagram} rel="noopener noreferrer" target="_blank">
+      <FaInstagram />
+    </a>
+    <a href={Bio.URLs.Github} rel="noopener noreferrer" target="_blank">
+      <FaGithub />
+    </a>
+    <a href={Bio.URLs.GoogleScholar} rel="noopener noreferrer" target="_blank">
+      <SiGooglescholar />
+    </a>
+    <a href={"mailto:" + Bio.URLs.Email} rel="noopener noreferrer" target="_blank">
+      <IoMail />
+    </a>
+    </div>
+
     </div>
     </Col>
     );
---
+++ src/data/bio.js
@@ -0,0 +1,15 @@
+export const Bio = {
+    Name: "Kaustubh Hiware",
+    Role: "Software Engineer",
+    Location: "Mercari US@Tokyo",
+    URLs: {
+        Twitter: "https://twitter.com/_ofkaus",
+        LinkedIn: "https://www.linkedin.com/in/ofkaus/",
+        Instagram: "https://instagram.com/_ofkaus",
+        Github: "https://github.com/kaustubhhiware",
+        GoogleScholar: "https://scholar.google.com/citations?hl=en&user=9iIiFjMAAAAJ",
+        Email: "hiwarekaustubh+web@gmail.com",
+    }
+};
+
+export default Bio;
