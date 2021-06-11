04512b8447d878c53869f403c8f65e84fd8a7eb4
Kaustubh Hiware <hiwarekaustubh@gmail.com>
2021-06-12 04:40:36 +0900

    Did you say more themes?

--- src/data/_themes.js
+++ src/data/_themes.js
@@ -6,16 +6,6 @@ ref: https://stackoverflow.com/a/42331003
 */
 export const Themes = [
     {
-        Name: "sakura",
-        Emoji: "🌸",
-        Colors: {
-            BG: "#F5E8ED",
-            Text: "black",
-            Shadow: "#E83261",
-        },
-        Source: "https://unsplash.com/photos/bBWHtMOvxKY",
-    },
-    {
         Name: "waves",
         Emoji: "🌊",
         Colors: {
@@ -24,6 +14,16 @@ export const Themes = [
             Shadow: "#346A84",
         },
         Source: "https://wallpapercave.com/the-great-wave-off-kanagawa-hd-wallpapers",
+    },
+    {
+        Name: "sakura",
+        Emoji: "🌸",
+        Colors: {
+            BG: "#F5E8ED",
+            Text: "black",
+            Shadow: "#E83261",
+        },
+        Source: "https://unsplash.com/photos/bBWHtMOvxKY",
     },
     {
         Name: "train",
@@ -35,10 +35,50 @@ export const Themes = [
         },
         Source: "https://unsplash.com/photos/PHyF2mCMei0",
     },
+    {
+        Name: "mountain",
+        Emoji: "⛰️",
+        Colors: {
+            BG: "#EDEBEC",
+            Text: "#327B8E",
+            Shadow: "#013C48",
+        },
+        Source: "https://unsplash.com/photos/bBL4o4BJxag",
+    },
+    {
+        Name: "beach",
+        Emoji: "🏖️",
+        Colors: {
+            BG: "#D9E0E9",
+            Text: "#002324",
+            Shadow: "#00404A",
+        },
+        Source: "https://unsplash.com/photos/bBL4o4BJxag",
+    },
+    {
+        Name: "snow",
+        Emoji: "❄️",
+        Colors: {
+            BG: "#1E2636",
+            Text: "#FEB79C",
+            Shadow: "#4F7091",
+        },
+        Source: "https://unsplash.com/photos/jBxee1u2RAU",
+    },
+    {
+        Name: "plant",
+        Emoji: "🌱",
+        Colors: {
+            BG: "#519D7B",
+            Text: "#06231F",
+            Shadow: "#073127",
+        },
+        Source: "https://unsplash.com/photos/Gl6GljPtJpo",
+    },
 ];
 
 
 
 
 export default Themes;
-export const ThemeDefault = "0";
+export const ThemeDefault = "6";
