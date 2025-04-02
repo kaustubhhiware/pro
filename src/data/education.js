8e58830bd06760d719a30db19cb6b5e21898fdea
Kaustubh Hiware <hiwarekaustubh@gmail.com>
2025-04-02 16:11:52 +0900

    education: minor. change

--- src/data/education.js
+++ src/data/education.js
@@ -6,7 +6,7 @@ export const Education = [
         When: "Jul 2014 - Jul 2019",
         Name: "Indian Institute of Technology, Kharagpur",
         URL: "http://www.iitkgp.ac.in/",
-        Degree: "B.Tech. & M.Tech. Hons. in Computer Science & Engineering",
+        Degree: "B.Tech & M.Tech in Computer Science & Engineering",
         Description: [
             "Awarded Honorable Mention for contributions in Technology at Annual Gymkhana awards.",
             "Served as Maintainer for MetaKGP during my final & pre-final year: a loose collaborative association of engineers & hackers",
